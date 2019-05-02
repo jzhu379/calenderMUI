c1a600c3f715b4bcadd55bb26371e4ffb219d361
jzhu379 <jzhu379@gmail.com>
2019-05-01 23:46:47 -0400

    lmao

--- src/Calender.js
+++ src/Calender.js
@@ -3,50 +3,50 @@ import {DatePicker} from 'material-ui-pickers';
 
 class Calender extends PureComponent
 {
-    state = {selectedDate: null}
+  state = {selectedDate: null}
 
-    getMinDate = () =>
-    {
-        const date = new Date();
-        let minDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 2);
-        return minDate;
-    }
+  getMinDate = () =>
+  {
+    const date = new Date();
+    let minDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 2);
+    return minDate;
+  }
 
-    getMaxDate = () =>
-    {
-        const date = new Date();
-        let maxDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 28);
-        return maxDate;
-    }
+  getMaxDate = () =>
+  {
+    const date = new Date();
+    let maxDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 28);
+    return maxDate;
+  }
 
-    dateToString = (date) =>
-    {
-        const tempDate = date.toDate();
-        return [tempDate.getFullYear(), tempDate.getMonth() + 1, tempDate.getDate()].join(' / ');
-    }
+  dateToString = (date) =>
+  {
+    const tempDate = date.toDate();
+    return [tempDate.getFullYear(), tempDate.getMonth() + 1, tempDate.getDate()].join(' / ');
+  }
 
-    dateChangeHandler = (date) => {this.setState({selectedDate: date});}
+  dateChangeHandler = (date) => {this.setState({selectedDate: date});}
 
-    render()
-    {
-        let msg = (this.state.selectedDate === null) ? 'select a date above ↑' : 'selected date (Y / M / D): ' + this.dateToString(this.state.selectedDate);
+  render()
+  {
+    let msg = (this.state.selectedDate === null) ? 'select a date above ↑' :
+      'selected date (Y / M / D): ' + this.dateToString(this.state.selectedDate);
 
-        return (
-            <div>
-                <DatePicker
-                label = 'SELECT DATE:'
-                minDateMessage = 'reservations must be made 2 days in advance'
-                maxDateMessage = 'reservations past 2 weeks cannot be made'
-                value = {this.state.selectedDate}
-                onChange = {this.dateChangeHandler}
-                maxDate = {this.getMaxDate()}
-                minDate = {this.getMinDate()}
-                />
-
-                <p> {msg} </p>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <DatePicker
+        label = 'SELECT DATE:'
+        minDateMessage = 'reservations must be made 2 days in advance'
+        maxDateMessage = 'reservations past 2 weeks cannot be made'
+        value = {this.state.selectedDate}
+        onChange = {this.dateChangeHandler}
+        maxDate = {this.getMaxDate()}
+        minDate = {this.getMinDate()}
+        />
+        <p> {msg} </p>
+      </div>
+    );
+  }
 }
 
 export default Calender;
